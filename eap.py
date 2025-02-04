ae6970ad814cd0deced972dad2cd9c662f049b3e
dsa_cat <113278747+dsacat@users.noreply.github.com>
2025-02-04 20:42:03 +0300

    Update eap.py

--- eap.py
+++ eap.py
@@ -1,6 +1,8 @@
 def fis(l, from_system=10, in_system=2, s=None, alf="0123456789ABCDEF"):
   if type(alf) != str:
     raise ValueError("The alf input data must be of the string type.")
+  if type(s) != str and s != None:
+    raise ValueError("The s input data must be of the string or None type.")
   if type(l) != int and type(l) != list:
     raise ValueError("The input data type of the variable l must be a list with integers or an integer.")
   if type(in_system) != int and type(in_system) != list:
@@ -22,3 +24,7 @@ def fis(l, from_system=10, in_system=2, s=None, alf="0123456789ABCDEF"):
   if type(in_system) != int:
     if len(in_system) != len(l):
       raise ValueError("the length of the input data of the in_system variable must be equal to the length of the input data of the l variable.")
+  if type(from_system) != int:
+    if len(from_system) != len(l):
+      raise ValueError("the length of the input data of the from_system variable must be equal to the length of the input data of the l variable.")
+  if type(l)
