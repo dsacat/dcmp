f2b082f94873464a97fa6e8f96ef5b2cea56c9bf
dsa_cat <113278747+dsacat@users.noreply.github.com>
2025-02-07 16:29:31 +0300

    Update eap.py

--- dsa_cat_all/eap.py
+++ dsa_cat_all/eap.py
@@ -1,9 +1,11 @@
 def fis(l, from_system=10, in_system=2, s=None, alf="0123456789ABCDEF"):
-  if type(alf) != str:
+  if type(alf) != list or alf != str:
     raise ValueError("The alf input data must be of the string type.")
+  elif type(alf) == str:
+    alf = list(alf)
   if type(s) != str and s != None:
     raise ValueError("The s input data must be of the string or None type.")
-  if type(l) != int and type(l) != list:
+  if type(l) != int and type(l) != list and type(l) != str:
     raise ValueError("The input data type of the variable l must be a list with integers or an integer.")
   if type(in_system) != int and type(in_system) != list:
     raise ValueError("The input data type of the variable in_system must be a list with integers or an integer.")
@@ -27,3 +29,22 @@ def fis(l, from_system=10, in_system=2, s=None, alf="0123456789ABCDEF"):
   if type(from_system) != int:
     if len(from_system) != len(l):
       raise ValueError("the length of the input data of the from_system variable must be equal to the length of the input data of the l variable.")
+  if type(l) == list:
+    l = list(map(str, l))
+  if type(l) == int:
+    l = [str(l)]
+  if type(l) == str:
+    l = [l]
+  if type(in_system) != int:
+    in_system = [in_system]*len(l)
+  if type(from_system) != int:
+    in_system = [from_system]*len(l)
+  l2 = []
+  for i in l:
+    g = 0
+    for i2, i3, i4 in zip(reversed(i), len(i), from_system):
+      try:
+        g+=i2*(i4**i3):
+      except:
+        raise ValueError("The item in the l list is not in the alf list.")
+  return g
