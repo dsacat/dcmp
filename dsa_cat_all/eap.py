3b2adc1ab498e56834b44331f4fd186b042c53d5
dsa_cat <113278747+dsacat@users.noreply.github.com>
2025-02-08 17:16:18 +0300

    Update eap.py

--- dsa_cat_all/eap.py
+++ dsa_cat_all/eap.py
@@ -1,20 +1,40 @@
-def fis(l, from_system=10, in_system=2, alf="0123456789ABCDEF", s=None, k=False):
+def fis(l, from_system=10, in_system=2, alf="0123456789ABCDEF", alf2=None, s=None, k=False):
   if type(alf) != list and type(alf) != str:
     raise ValueError("The alf input data must be a string or a list.")
-  elif type(alf) == str:
+  if type(alf) == str:
     alf = list(alf)
+  if type(alf2) != list and type(alf2) != str and alf2 != None:
+    raise ValueError("The alf input data must be a string or a list or None type.")
+  if type(alf2) == str:
+    alf = list(alf2)
+  if alf2 == None:
+    alf2 = alf
   if type(s) != str and s != None:
     raise ValueError("The s input data must be of the string or None type.")
   if type(l) != int and type(l) != list and type(l) != str:
-    raise ValueError("The input data type of the variable l must be a list with integers or an integer.")
+    raise ValueError("The input data type of the variable l must be a list with integers or a list with list ith integers or an integer.")
   if type(in_system) != int and type(in_system) != list:
     raise ValueError("The input data type of the variable in_system must be a list with integers or an integer.")
   if type(from_system) != int and type(from_system) != list:
     raise ValueError("The input data type of the variable from_system must be a list with integers or an integer.")
+  p = False
   if type(l) == list:
     for i in l:
-      if type(i) != int:
-        raise ValueError("The input data type of the variable l must be a list with integers or an integer.")
+      if type(i) != str and type(i) != int and type(i) != list:
+        raise ValueError("The input data type of the variable l must be a list with integers or a list with list ith integers or an integer.")
+      if type(i)==list:
+        p=True
+        for i2 in i:
+          if type(i2) != int and type(i2) != str:
+              raise ValueError("The input data type of the variable l must be a list with integers or a list with list ith integers or an integer.")
+    if p == True:
+      for i in l:
+        if type(i) != list:
+          raise ValueError("The input data type of the variable l must be a list with integers or a list with list ith integers or an integer.")
+    if p == False:
+      for i in l:
+        if type(i) != str and type(i) != int:
+          raise ValueError("The input data type of the variable l must be a list with integers or a list with list ith integers or an integer.")
   if type(in_system) == list:
     for i in in_system:
       if type(i) != int:
@@ -30,7 +50,13 @@ def fis(l, from_system=10, in_system=2, alf="0123456789ABCDEF", s=None, k=False)
     if len(from_system) != len(l):
       raise ValueError("the length of the input data of the from_system variable must be equal to the length of the input data of the l variable.")
   if type(l) == list:
-    l = list(map(str, l))
+    if p == False:
+      l = list(map(str, l))
+    if p == True:
+      l2 = []
+      for i in l:
+        l2 += [list(map(str, i))]
+      l = l2
   if type(l) == int:
     l = [str(l)]
   if type(l) == str:
@@ -44,7 +70,7 @@ def fis(l, from_system=10, in_system=2, alf="0123456789ABCDEF", s=None, k=False)
     g = 0
     for i2, i3 in zip(list(reversed(i)), range(len(i))):
       try:
-        g+=alf.index(i2)*(i4**i3)
+        g+=alf2.index(i2)*(i4**i3)
       except:
         raise ValueError("The item in the l list is not in the alf list.")
     l2+=[g]
@@ -63,7 +89,7 @@ def fis(l, from_system=10, in_system=2, alf="0123456789ABCDEF", s=None, k=False)
       except:
         raise ValueError("The item in the l list is not in the alf list.")
   else:
-    l = []*len(l2)
+    l = [[]]*len(l2)
     for i, i2, i3 in zip(l2, in_system, range(len(l2))):
       m = i
       while m // i2 != 0:
